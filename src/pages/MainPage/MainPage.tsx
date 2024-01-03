f168e4846b3d8b1a71116b8bfbef8c53bb2b01b2
Aleksejs Kanarciks <kotik90@gmail.com>
2024-01-03 20:36:41 +0200

    feat(17): replace semantic tags with UUI components Part 2

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,4 +1,12 @@
-import { FlexRow, Button, Badge, Text, LinkButton } from '@epam/uui';
+import {
+  FlexRow,
+  Button,
+  Badge,
+  Text,
+  LinkButton,
+  StatusIndicator,
+  Panel,
+} from '@epam/uui';
 import { FlexCell } from '@epam/uui-components';
 import styles from './MainPage.module.scss';
 import { mockBlocks } from './mocks/mockBlocks';
@@ -21,7 +29,7 @@ export const MainPage = () => {
       {/* How it works section */}
       <FlexRow cx={styles.sectionAlignment}>
         <FlexCell width="100%">
-          <FlexRow cx={styles.sectionHeading}>
+          <FlexRow cx={styles.sectionHeadline}>
             <h3>How it works:</h3>
           </FlexRow>
           {/* TODO: ALIGN */}
@@ -52,64 +60,78 @@ export const MainPage = () => {
       {/* Current tenders */}
       <FlexRow cx={styles.sectionAlignment}>
         <FlexCell width="100%">
-          <FlexRow cx={styles.sectionHeading}>
+          <FlexRow cx={styles.sectionHeadline}>
             <h3>Current tenders</h3>
-            <LinkButton caption="View all tenders" link={{ pathname: '/' }} />
+            <LinkButton
+              caption="View all tenders"
+              link={{ pathname: '/' }}
+              size="42"
+            />
           </FlexRow>
-          <div className={styles.wrapper}>
+          <Panel cx={styles.sectionContentWrapper}>
             {tenders.map((tender) => (
-              <div key={tender.id} className={styles.tenderCard}>
-                <div className={styles.meta}>
-                  <div className={styles.leftColumn}>
-                    <div className={styles.firstRow}>
+              <FlexCell key={tender.id} cx={styles.tenderCard}>
+                <FlexRow cx={styles.meta}>
+                  <FlexCell grow={1} textAlign="left">
+                    <FlexRow cx={styles.firstRow}>
                       {tender.tags.map((tag) => (
-                        <span key={tag} className={styles.tag}>
-                          {tag}
-                        </span>
+                        <Badge
+                          key={tag}
+                          color="neutral"
+                          fill="solid"
+                          caption={tag}
+                          size="18"
+                          cx={styles.tag}
+                        />
                       ))}
-                      <div className={styles.verticalDivider} />
-                      <div className={styles.location}>
+                      <FlexRow cx={styles.location}>
                         <GeoLocationIcon />
                         {tender.location}
-                      </div>
-                    </div>
-                    <div className={styles.secondRow}>
-                      <div className={styles.author}>
+                      </FlexRow>
+                    </FlexRow>
+                    <FlexRow cx={styles.secondRow}>
+                      <Text cx={styles.author}>
                         Posted by: <span>{tender.author}</span>
-                      </div>
-                      {/* TODO: make dot as a separated reusable component */}
-                      <div className={styles.dot} />
-                      <div className={styles.attachments}>
-                        <AttachmentIcon />
-                        <span>3</span>
-                      </div>
-                      <div className={styles.dot} />
-                      <div className={styles.postedDate}>
+                      </Text>
+                      <StatusIndicator
+                        size="18"
+                        caption=""
+                        cx={styles.statusIndicator}
+                      />
+                      <Badge
+                        size="24"
+                        color="neutral"
+                        fill="solid"
+                        icon={AttachmentIcon}
+                        caption={tender.attachments.length}
+                      />
+                      <StatusIndicator
+                        size="18"
+                        caption=""
+                        cx={styles.statusIndicator}
+                      />
+                      <Text cx={styles.postedDate}>
                         Posted: <span>{tender.published}</span>
-                      </div>
-                    </div>
-                  </div>
-                  <div className={styles.rightColumn}>
-                    <div className={styles.status}>
-                      <div
-                        className={styles.dot}
-                        style={{
-                          backgroundColor:
-                            tender.status === 'Submission'
-                              ? '#0954A5'
-                              : '#B114D1',
-                        }}
+                      </Text>
+                    </FlexRow>
+                  </FlexCell>
+                  <FlexCell grow={1} textAlign="right">
+                    <FlexRow cx={styles.status}>
+                      <StatusIndicator
+                        size="18"
+                        caption=""
+                        cx={styles.statusIndicator}
                       />
                       Proposals {tender.status}
-                    </div>
-                    <div className={styles.delivery}>
+                    </FlexRow>
+                    <FlexRow cx={styles.delivery}>
                       Expected delivery of work: <span>{tender.delivery}</span>
-                    </div>
-                  </div>
-                </div>
-                <div>
-                  <p className={styles.tenderTitle}>{tender.title}</p>
-                  <p className={styles.description}>{tender.description}</p>
+                    </FlexRow>
+                  </FlexCell>
+                </FlexRow>
+                <FlexCell>
+                  <Text cx={styles.tenderTitle}>{tender.title}</Text>
+                  <Text cx={styles.description}>{tender.description}</Text>
                   {tender.status === 'Submission' && (
                     <Button
                       color="primary"
@@ -117,31 +139,33 @@ export const MainPage = () => {
                       onClick={() => null}
                     />
                   )}
-                </div>
+                </FlexCell>
                 {/* TODO: Small proposals card component */}
                 {tender.status === 'Voting' && (
                   <div className={styles.submittedProposals}>
-                    <p className={styles.proposalsTitle}>
-                      Submitted proposals ({tender.proposals.length})
-                    </p>
-                    <div className={styles.proposals}>
+                    <FlexRow>
+                      <h5 className={styles.proposalsTitle}>
+                        Submitted proposals ({tender.proposals.length})
+                      </h5>
+                    </FlexRow>
+                    <FlexRow>
                       {tender.proposals.slice(0, 2).map((proposal) => (
-                        <div
+                        <FlexRow
                           key={proposal.id}
-                          className={styles.smallProposalCard}
+                          cx={styles.smallProposalCard}
                         >
                           <img src={`images/${proposal.thumbnail}_small.png`} />
-                          <div className={styles.proposalInfo}>
-                            <div className={styles.proposalLikes}>
+                          <FlexCell grow={1} cx={styles.proposalInfo}>
+                            <FlexRow cx={styles.proposalLikes}>
                               <ThumbUpIcon />
                               {proposal.likes}
-                            </div>
-                            <div className={styles.proposalLink}>
-                              {proposal.title}
-                            </div>
-                            <p className={styles.proposalAuthorTitle}>
-                              Author:
-                            </p>
+                            </FlexRow>
+                            <LinkButton
+                              caption={proposal.title}
+                              link={{ pathname: '/' }}
+                              size="42"
+                            />
+                            <Text cx={styles.proposalAuthorTitle}>Author:</Text>
                             {proposal.authors.map((author) => (
                               <div
                                 key={author.id}
@@ -169,29 +193,33 @@ export const MainPage = () => {
                                 </div>
                               </div>
                             ))}
-                          </div>
-                        </div>
+                          </FlexCell>
+                        </FlexRow>
                       ))}
-                    </div>
+                    </FlexRow>
                   </div>
                 )}
                 {/* TODO: MORE PROPOSALS LINK */}
-              </div>
+              </FlexCell>
             ))}
-          </div>
+          </Panel>
         </FlexCell>
       </FlexRow>
       {/* Current proposals */}
       <FlexRow cx={styles.sectionAlignment}>
         <FlexCell width="100%">
-          <div className={styles.sectionHeader}>
+          <FlexRow cx={styles.sectionHeadline}>
             <h3>Current proposals</h3>
-            <a href="#">View all proposals</a>
-          </div>
-          <div className={styles.wrapper}>
+            <LinkButton
+              caption="View all proposals"
+              link={{ pathname: '/' }}
+              size="42"
+            />
+          </FlexRow>
+          <Panel cx={styles.sectionContentWrapper}>
             {tenders.map((tender) =>
               tender.proposals.slice(0, 3).map((proposal) => (
-                <div key={proposal.id} className={styles.bigProposalCard}>
+                <FlexCell key={proposal.id} cx={styles.bigProposalCard}>
                   <img
                     className={styles.bigProposalCardThumb}
                     src={`images/${proposal.thumbnail}_big.png`}
@@ -208,7 +236,12 @@ export const MainPage = () => {
                         </div>
                         <div className={styles.secondRow}>
                           <div className={styles.related}>
-                            Related to tender: <span>{tender.title}</span>
+                            Related to tender:
+                            <LinkButton
+                              caption={tender.title}
+                              link={{ pathname: '/' }}
+                              size="42"
+                            />
                           </div>
                         </div>
                       </div>
@@ -316,10 +349,10 @@ export const MainPage = () => {
                       />
                     </div>
                   </div>
-                </div>
+                </FlexCell>
               )),
             )}
-          </div>
+          </Panel>
         </FlexCell>
       </FlexRow>
     </>
--- src/pages/MainPage/mocks/mockTenders.ts
+++ src/pages/MainPage/mocks/mockTenders.ts
@@ -4,7 +4,6 @@ export const tenders = [
     tags: ['Sculpture', 'Outdoors'],
     location: 'Montenegro, Kotor',
     author: 'Regional Office',
-    attachments: 3,
     published: '11 Nov 2023',
     delivery: '15 Feb 2024',
     status: 'Submission',
@@ -12,13 +11,14 @@ export const tenders = [
     description:
       'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.',
     proposals: [],
+    attachments: ['1', '2'],
   },
   {
     id: 12,
     tags: ['Sculpture', 'Outdoors'],
     location: 'Montenegro, Kotor',
     author: 'Regional Office',
-    attachments: 3,
+    attachments: ['1', '2', '3'],
     published: '11 Nov 2023',
     delivery: '15 Feb 2024',
     status: 'Voting',
