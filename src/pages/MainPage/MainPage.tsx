dd829efb3f4f08c50bd5354d1d52a725935a5876
Aleksejs Kanarciks <kotik90@gmail.com>
2024-01-04 11:53:24 +0200

    feat(17): replace semantic tags with UUI components Part 3

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -7,7 +7,7 @@ import {
   StatusIndicator,
   Panel,
 } from '@epam/uui';
-import { FlexCell } from '@epam/uui-components';
+import { FlexCell, FlexSpacer } from '@epam/uui-components';
 import styles from './MainPage.module.scss';
 import { mockBlocks } from './mocks/mockBlocks';
 import { tenders } from './mocks/mockTenders';
@@ -32,11 +32,11 @@ export const MainPage = () => {
           <FlexRow cx={styles.sectionHeadline}>
             <h3>How it works:</h3>
           </FlexRow>
-          {/* TODO: ALIGN */}
+          {/* TODO: VERTICAL ALIGN */}
           <FlexCell>
             {mockBlocks.map((block, index) => (
               <FlexRow key={block.id} cx={styles.blockWrapper}>
-                <div className={styles.block}>
+                <Panel cx={styles.block}>
                   <FlexRow>
                     <h2 className={styles.blockNumber}>
                       {(index + 1).toString().padStart(2, '0')}.
@@ -50,7 +50,7 @@ export const MainPage = () => {
                       {block.description}
                     </Text>
                   </FlexRow>
-                </div>
+                </Panel>
               </FlexRow>
             ))}
           </FlexCell>
@@ -142,7 +142,7 @@ export const MainPage = () => {
                 </FlexCell>
                 {/* TODO: Small proposals card component */}
                 {tender.status === 'Voting' && (
-                  <div className={styles.submittedProposals}>
+                  <FlexCell cx={styles.submittedProposals}>
                     <FlexRow>
                       <h5 className={styles.proposalsTitle}>
                         Submitted proposals ({tender.proposals.length})
@@ -167,9 +167,9 @@ export const MainPage = () => {
                             />
                             <Text cx={styles.proposalAuthorTitle}>Author:</Text>
                             {proposal.authors.map((author) => (
-                              <div
+                              <FlexCell
                                 key={author.id}
-                                className={styles.proposalAuthorInfo}
+                                cx={styles.proposalAuthorInfo}
                               >
                                 {proposal.authors.length === 1 && (
                                   <img
@@ -177,29 +177,43 @@ export const MainPage = () => {
                                     src={`images/${author.authorPic}`}
                                   />
                                 )}
-                                <div className={styles.proposalAuthorData}>
-                                  <p className={styles.proposalAuthorName}>
+                                <FlexCell
+                                  width="auto"
+                                  cx={styles.proposalAuthorData}
+                                >
+                                  <Text cx={styles.proposalAuthorName}>
                                     {author.authorName}
-                                  </p>
+                                  </Text>
                                   {proposal.authors.length === 1 && (
-                                    <p className={styles.proposalAuthorRole}>
+                                    <Text cx={styles.proposalAuthorRole}>
                                       {author.authorRole}
-                                    </p>
+                                    </Text>
                                   )}
-                                </div>
-                                <div className={styles.proposalAuthorSocials}>
+                                </FlexCell>
+                                <FlexCell
+                                  width="auto"
+                                  cx={styles.proposalAuthorSocials}
+                                >
                                   <LinkedInIcon />
                                   <EmailIcon />
-                                </div>
-                              </div>
+                                </FlexCell>
+                              </FlexCell>
                             ))}
                           </FlexCell>
                         </FlexRow>
                       ))}
                     </FlexRow>
-                  </div>
+                  </FlexCell>
                 )}
-                {/* TODO: MORE PROPOSALS LINK */}
+                <FlexRow>
+                  <FlexSpacer />
+                  <LinkButton
+                    caption="MORE PROPOSALS"
+                    link={{ pathname: '/' }}
+                    size="42"
+                  />
+                  <FlexSpacer />
+                </FlexRow>
               </FlexCell>
             ))}
           </Panel>
@@ -224,47 +238,42 @@ export const MainPage = () => {
                     className={styles.bigProposalCardThumb}
                     src={`images/${proposal.thumbnail}_big.png`}
                   />
-                  <div
-                    style={{ width: '100%', padding: '18px 24px 24px 24px' }}
-                  >
-                    <div className={styles.meta}>
-                      <div className={styles.leftColumn}>
-                        <div className={styles.firstRow}>
-                          <div className={styles.postedDate}>
+                  <Panel cx={styles.bigProposalCardContent}>
+                    <FlexRow cx={styles.meta}>
+                      <FlexCell grow={1} textAlign="left">
+                        <FlexRow cx={styles.firstRow}>
+                          <Text cx={styles.postedDate}>
                             Submitted: <span>{proposal.published}</span>
-                          </div>
-                        </div>
-                        <div className={styles.secondRow}>
-                          <div className={styles.related}>
+                          </Text>
+                        </FlexRow>
+                        <FlexRow cx={styles.secondRow}>
+                          <Text cx={styles.related}>
                             Related to tender:
                             <LinkButton
                               caption={tender.title}
                               link={{ pathname: '/' }}
                               size="42"
                             />
-                          </div>
-                        </div>
-                      </div>
-                      <div className={styles.rightColumn}>
-                        <div className={styles.status}>
-                          <div
-                            className={styles.dot}
-                            style={{
-                              backgroundColor:
-                                proposal.status === 'Proposals Voting'
-                                  ? '#B114D1'
-                                  : proposal.status === 'Winner Selection'
-                                  ? '#773CEC'
-                                  : '#FDD63B',
-                            }}
+                          </Text>
+                        </FlexRow>
+                      </FlexCell>
+                      <FlexCell grow={1} textAlign="right">
+                        <FlexRow cx={styles.status}>
+                          <StatusIndicator
+                            size="18"
+                            caption=""
+                            cx={styles.statusIndicator}
                           />
                           {proposal.status}
-                        </div>
-                      </div>
-                    </div>
-                    <div className={styles.thirdRow}>
-                      <div className={styles.proposalCardTitleWrapper}>
-                        <p className={styles.tenderTitle}>{proposal.title}</p>
+                        </FlexRow>
+                      </FlexCell>
+                    </FlexRow>
+                    <FlexRow cx={styles.thirdRow}>
+                      <FlexCell
+                        cx={styles.proposalCardTitleWrapper}
+                        width="100%"
+                      >
+                        <Text cx={styles.tenderTitle}>{proposal.title}</Text>
                         {proposal.selected && (
                           <Badge
                             icon={MedalIcon}
@@ -274,46 +283,50 @@ export const MainPage = () => {
                             size="18"
                           />
                         )}
-                      </div>
-                      <div
-                        className={`${styles.proposalLikes} ${
+                      </FlexCell>
+                      <Text
+                        cx={`${styles.proposalLikes} ${
                           proposal.liked && styles.liked
                         }`}
                       >
                         <ThumbUpIcon />
                         {proposal.likes}
-                      </div>
-                    </div>
-                    <p className={styles.description}>{proposal.description}</p>
-                    <p className={styles.proposalAuthorTitle}>Author:</p>
-                    <div className={styles.bigCardProposalAuthorWrapper}>
+                      </Text>
+                    </FlexRow>
+                    <Text cx={styles.description}>{proposal.description}</Text>
+                    <Text cx={styles.proposalAuthorTitle}>Author:</Text>
+                    <FlexCell cx={styles.bigCardProposalAuthorWrapper}>
                       {proposal.authors.map((author) => (
-                        <div
+                        <FlexCell
                           key={author.id}
-                          className={styles.proposalAuthorInfo}
+                          cx={styles.proposalAuthorInfo}
+                          width="auto"
                         >
                           <img
                             className={styles.proposalAuthorPic}
                             src={`images/${author.authorPic}`}
                           />
 
-                          <div className={styles.proposalAuthorData}>
-                            <p className={styles.proposalAuthorName}>
+                          <FlexCell width="auto" cx={styles.proposalAuthorData}>
+                            <Text cx={styles.proposalAuthorName}>
                               {author.authorName}
-                            </p>
+                            </Text>
 
-                            <p className={styles.proposalAuthorRole}>
+                            <Text cx={styles.proposalAuthorRole}>
                               {author.authorRole}
-                            </p>
-                          </div>
-                          <div className={styles.proposalAuthorSocials}>
+                            </Text>
+                          </FlexCell>
+                          <FlexCell
+                            width="auto"
+                            cx={styles.proposalAuthorSocials}
+                          >
                             <LinkedInIcon />
                             <EmailIcon />
-                          </div>
-                        </div>
+                          </FlexCell>
+                        </FlexCell>
                       ))}
-                    </div>
-                    <div className={styles.proposalCtas}>
+                    </FlexCell>
+                    <FlexCell cx={styles.proposalCtas}>
                       {proposal.status === 'Fundraising' && (
                         <Button
                           color="primary"
@@ -347,8 +360,8 @@ export const MainPage = () => {
                         color="secondary"
                         onClick={() => null}
                       />
-                    </div>
-                  </div>
+                    </FlexCell>
+                  </Panel>
                 </FlexCell>
               )),
             )}
--- src/pages/MainPage/components/Header/Header.tsx
+++ src/pages/MainPage/components/Header/Header.tsx
@@ -24,6 +24,7 @@ export const Header = () => {
           />
         </FlexRow>
       </FlexCell>
+      {/* TODO: ADD PICTURE */}
     </FlexRow>
   );
 };
